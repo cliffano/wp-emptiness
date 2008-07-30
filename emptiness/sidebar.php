fc988a3d1225a121e6bdb49864cd972139f748e6
cliffano <cliffano@gmail.com>
2008-07-30 15:37:25 +0000

    Updated sidebars to be widget ready as requested http://blog.qoqoa.com/2008/05/31/emptiness-theme-v01/#comment-499 . Bumped version up to 0.2 for release.

--- emptiness/sidebar.php
+++ emptiness/sidebar.php
@@ -1,16 +1,18 @@
         <div class="side right">
-          <h3>Recent Posts</h3>
-          <ul>
-            <?php wp_get_archives('type=postbypost&limit=10'); ?> 
-          </ul>
-          <?php if (function_exists('get_recent_comments')) { ?>
-            <h4>Recent Comments</h4>
+          <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Body Right Sidebar') ) : ?>
+            <h3>Recent Posts</h3>
             <ul>
-              <?php get_recent_comments(); ?>
+              <?php wp_get_archives('type=postbypost&limit=10'); ?> 
             </ul>
-          <?php } ?>
-	        <h4>Linkroll</h4>
-		      <ul>
-			      <?php get_links(-1, '<li>', '</li>', ' - '); ?>
-		      </ul>
+            <?php if (function_exists('get_recent_comments')) { ?>
+              <h3>Recent Comments</h3>
+              <ul>
+                <?php get_recent_comments(); ?>
+              </ul>
+            <?php } ?>
+	        <h3>Linkroll</h3>
+		    <ul>
+			  <?php get_links(-1, '<li>', '</li>', ' - '); ?>
+		    </ul>
+		  <?php endif; ?>
         </div>
