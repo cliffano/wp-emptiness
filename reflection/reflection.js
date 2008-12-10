bdf095a8bb05e3d28839f3b6ef55a7c2af7ce1a0
cliffano <cliffano@gmail.com>
2008-12-10 12:23:33 +0000

    Tweaked reflection coordinates and dimension for best cross-browser display.

--- reflection/reflection.js
+++ reflection/reflection.js
@@ -15,14 +15,16 @@ function BitpressEffectMgr(image, height) {
 	}
 	this.raphael = new Raphael(this.image.parentNode, this.image.width, this.image.height + this.reflectionHeight);
 }
+// Note: the reflection image coordinates and dimension have been optimised for IE, FF, and Opera.
 BitpressEffectMgr.prototype.displayDefaultReflection = function() {
 	this.raphael.image(this.image.src, 0, 0, this.image.width, this.image.height);
-	this.raphael.image(this.image.src, 0, this.image.height, this.image.width, this.image.height).scale(1, -1).attr({opacity: .5});
+	this.raphael.image(this.image.src, 0, this.image.height - 1, this.image.width + 1, this.image.height).scale(1, -1).attr({opacity: .5});
 }
+// Note: the gradient rectangle coordinates and dimension have been optimised for IE, FF, and Opera.
 BitpressEffectMgr.prototype.displayGradientReflection = function(color) {
 	this.displayDefaultReflection();
 	var gradient = { type: "linear", dots: [{color: color, opacity: .5}, {color: color}], vector: [0, 0, 0, "100%"] };
-    this.raphael.rect(0, this.image.height, this.image.width, this.reflectionHeight + 1).attr({gradient: gradient, stroke: null});
+    this.raphael.rect(-1, this.image.height - 1, this.image.width + 5, this.reflectionHeight + 5).attr({gradient: gradient, stroke: 'none'});
 }
 
 /*****************************************************************
@@ -39,26 +41,30 @@ BitpressImageMgr.prototype.process = function(images) {
 	    for (var j = 0; j < classNames.length; j++) {
 	    	if (classNames[j] == "reflection") {
 				this.applyEffect(images[i]);
-				images[i].width = 0;
-				images[i].height = 0;
 	    	}
 	    }
 	}
 }
 BitpressImageMgr.prototype.applyEffect = function(image) {
-	this.wrap(image);
+	this.insertDivWrapper(image);
     if (this.gradientBgColor != null && this.gradientHeight != null) {
     	(new BitpressEffectMgr(image, this.gradientHeight)).displayGradientReflection(this.gradientBgColor);
     } else {
     	(new BitpressEffectMgr(image)).displayDefaultReflection();
     }
+	this.hideOrigImage(image);
 }
-BitpressImageMgr.prototype.wrap = function(image) {
+// Inserts div between the image and its parent node.
+BitpressImageMgr.prototype.insertDivWrapper = function(image) {
 	var div = document.createElement("div");
 	var parent = image.parentNode;
 	parent.replaceChild(div, image);
 	div.appendChild(image);
+}
+// Hides the original image. Note: using style.display = "none" causes problem on IE.
+BitpressImageMgr.prototype.hideOrigImage = function(image) {
 	image.style.visibility = "hidden";
-	//image.style.display = "none";
 	image.parentNode.className = image.className;
+	image.width = 0;
+	image.height = 0;
 }
