bb4cc27aa62929eadee11f245509c8b6318a9779
cliffano <cliffano@gmail.com>
2008-12-27 05:56:44 +0000

    Emptiness - Added old logout construct for pre WP 2.7 backward compatibility.

--- emptiness/comments.php
+++ emptiness/comments.php
@@ -45,7 +45,11 @@
                   <?php if ( $user_ID ) : ?>
                     logged in as<br/>
                     <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a><br/>
-                    <a href="<?php echo wp_logout_url(); ?>">log out</a><br/>
+                    <?php if (function_exists('wp_logout_url')) { ?>
+	                    <a href="<?php echo wp_logout_url(); ?>">log out</a><br/>
+	                <?php } else { ?>
+	                    <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout">log out</a><br/>
+                    <?php } ?>
                   <?php else : ?>
                     *name<br/>
                     <input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="17" tabindex="1" /><br/>
