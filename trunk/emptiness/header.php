807273e3deab41a8b5eaf894ff59a931c514aea1
cliffano <cliffano@88037229-fb4d-0410-9201-6f9a1f84ae13>
2009-03-02 11:48:21 +0000

    Changed title separator to use &raquo; to better indicate structure.

--- trunk/emptiness/header.php
+++ trunk/emptiness/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
   <head>
-    <title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
     <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <style type="text/css" media="screen">
       @import url( <?php bloginfo('stylesheet_url'); ?> );
