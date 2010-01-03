1099d259d19b2b9d051ad0472073b6451759de1e
cliffano <cliffano@88037229-fb4d-0410-9201-6f9a1f84ae13>
2010-01-03 13:53:47 +0000

    Added paginated post support.

--- trunk/emptiness/src/main/php/index.php
+++ trunk/emptiness/src/main/php/index.php
@@ -19,6 +19,7 @@
                   <div class="main">
                     <h2><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                     <?php the_content('more &raquo;'); ?>
+                    <?php wp_link_pages('before=pages:&next_or_number=number&pagelink=%'); ?>
                   </div>
                 </div>
               <?php endwhile; ?>
