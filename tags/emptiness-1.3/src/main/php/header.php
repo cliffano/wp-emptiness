7dab9325c4622d7042bf80be46e6ccff50bff763
cliffano <cliffano@88037229-fb4d-0410-9201-6f9a1f84ae13>
2010-01-03 13:11:59 +0000

    [maven-release-plugin]  copy for tag emptiness-1.3

---
+++ tags/emptiness-1.3/src/main/php/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
+  <head>
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <style type="text/css" media="screen">
+      @import url( <?php bloginfo('stylesheet_url'); ?> );
+    </style>
+    <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <?php wp_get_archives('type=monthly&format=link'); ?>
+    <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+    <!-- Emptiness Theme by Studio Cliffano -->
+  </head>
+  <body>
