72c1274f9d46a09ab43a8c9399c78b5db232cfd3
cliffano <cliffano@88037229-fb4d-0410-9201-6f9a1f84ae13>
2010-01-12 07:33:00 +0000

    Cleaned up HTML markup, extracted template code from header.php and footer.php to index.php . header.php and footer.php now only contains the content of header and footer, and hence can be ordered as per content-first approach, i.e. index.php contains (1) body content, (2) a call to get_footer, (3) a call to get_header() .

--- trunk/emptiness/src/main/php/index.php
+++ trunk/emptiness/src/main/php/index.php
@@ -1,4 +1,24 @@
-    <?php get_header(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes('xhtml'); ?>>
+  <head>
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <style type="text/css" media="screen, projection">
+      @import url( <?php bloginfo('stylesheet_url'); ?> );
+    </style>
+    <style type="text/css" media="print">
+      @import url( <?php bloginfo('stylesheet_directory'); ?>/print.css );
+    </style>
+    <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <?php wp_get_archives('type=monthly&format=link'); ?>
+    <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+    <!-- Emptiness Theme by Studio Cliffano -->
+  </head>
+  <body>
     <div id="container">
       <div id="bodyfooter">
         <div id="body">
@@ -58,3 +78,11 @@
           <br style="clear: both;"/>
         </div>
         <?php get_footer(); ?>
+      </div>
+      <div id="header">
+        <?php get_header(); ?>
+      </div>
+    </div>
+    <?php wp_footer(); ?>
+  </body>
+</html>
