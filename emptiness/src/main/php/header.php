d0c7892bb32e2ce9a63dca16facfd763a69ab555
cliffano <cliffano@gmail.com>
2010-12-21 13:38:13 +0000

    Added nav_menu support, added post thumbnail support. Used esc_url for footer links.

--- emptiness/src/main/php/header.php
+++ emptiness/src/main/php/header.php
@@ -24,6 +24,7 @@
                 <?php } ?>
                 <li><?php wp_loginout(); ?></li>
               </ul>
+              <?php wp_nav_menu(); ?>
             <?php endif; ?>
           </div>
           <div class="main splash">
