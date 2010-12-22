14865681b5625ebbab84023bb0392d2716fe328e
cliffano <cliffano@88037229-fb4d-0410-9201-6f9a1f84ae13>
2010-12-22 10:23:11 +0000

    Tagged 1.13 .

---
+++ tags/emptiness-1.13/src/main/php/footer.php
@@ -0,0 +1,16 @@
+        <div id="footer">
+          <div class="item">
+            <div class="side left">
+              &nbsp;
+            </div>
+            <div class="main">
+              Copyright &copy; <?php echo date('Y'); ?>. Some Rights Reserved.<br/>
+              <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a> is powered by <a href="<?php echo esc_url( __('http://www.wordpress.org/', 'http://www.wordpress.org/') ); ?>">WordPress</a>.<br/>
+              <a href="<?php echo esc_url( __('http://code.google.com/p/bitpress/wiki/Emptiness', 'http://code.google.com/p/bitpress/wiki/Emptiness') ); ?>">Emptiness Theme</a> by <a href="<?php echo esc_url( __('http://cliffano.com', 'http://cliffano.com') ); ?>">Studio Cliffano</a>.
+            </div>
+            <div class="side right">
+              &nbsp;
+            </div>
+          </div>
+          <br style="clear: both;"/>
+        </div>
