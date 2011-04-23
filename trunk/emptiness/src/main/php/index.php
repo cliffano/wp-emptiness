0562c6f368d7480193c34b05fd24f9ea36029762
cliffano <cliffano@88037229-fb4d-0410-9201-6f9a1f84ae13>
2011-04-23 16:37:55 +0000

    Added comment link at the bottom of posts displayed on a list.

--- trunk/emptiness/src/main/php/index.php
+++ trunk/emptiness/src/main/php/index.php
@@ -41,6 +41,10 @@
                     <h2><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                     <?php the_post_thumbnail(); ?>
                     <?php the_content('more &raquo;'); ?>
+                    <?php if (!is_single()) : ?>
+                      <?php comments_popup_link('leave a comment', '1 comment', '% comments'); ?>
+                      <br/>
+                    <?php endif; ?>
                     <div class="nav">
                       <?php wp_link_pages('before=pages:&next_or_number=number&pagelink=%'); ?>
                     </div>
